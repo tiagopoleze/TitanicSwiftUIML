62d62c13f885d4f8a877d743232f9b6936e768a3
tiagopoleze <tiagopoleze@icloud.com>
2020-05-26 16:36:42 -0300

    Initial Commit

---
+++ TitanicSwiftUIML/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TitanicSwiftUIML
+//
+//  Created by Tiago Ferreira on 26/05/20.
+//  Copyright © 2020 Tiago Ferreira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
